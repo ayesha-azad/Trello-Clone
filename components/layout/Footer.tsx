70ab9c6066010eb9fa62a32fcd8fa4e20801eff9
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-02-09 11:26:56 +0500

    Checking CI and CD again and making sure push works

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Trello Clone. CI and CD by Ayesha Azad. All rights reserved.</p>
+          <p>&copy; 2024 &copy; Trello Clone. CI and CD Ayesha Azad.</p>
         </div>
       </div>
     </footer>
