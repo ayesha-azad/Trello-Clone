bb1858af3ad0a77bdd3b2517e23b560bbeb7a4b8
Ayesha Azad <ayeshaazad@Ayeshas-MacBook-Pro.local>
2026-02-09 10:20:34 +0500

    Checking CI and CD

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -97,7 +97,7 @@ export default function Footer() {
         </div>
 
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 Trello Clone. All rights reserved.</p>
+          <p>&copy; 2024 Trello Clone. All rights reserved. CI and CD by Ayesha Azad</p>
         </div>
       </div>
     </footer>
